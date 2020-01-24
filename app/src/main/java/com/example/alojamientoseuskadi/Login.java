4cfe44f6ceebb1fa2e4362584fc33fac2a04e239
AlexLara99 <33617910+AlexLara99@users.noreply.github.com>
2020-01-24 08:54:04 +0100

    inicio de sesión a través de la BBDD

--- app/src/main/java/com/example/alojamientoseuskadi/Login.java
+++ app/src/main/java/com/example/alojamientoseuskadi/Login.java
@@ -2,144 +2,162 @@ package com.example.alojamientoseuskadi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Login extends AppCompatActivity {
-    private List<Usuario> listaUsuarios;
-    private ParseJson parse1;
+
     private EditText usuario;
     private EditText contrasena;
-    private boolean usuarioCorrecto = false;
-    private boolean contrasenaCorrecta = false;
-    private String usuarioIntroducido;
-    private String contrasenaIntroducida;
+    private String logeado = "";
+    private TextView errorUsu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
 
-        usuario=(EditText)findViewById(R.id.contrasena1);
+        usuario=(EditText)findViewById(R.id.usuario);
         contrasena=(EditText)findViewById(R.id.contraseña);
 
-        //Se guarda el usuario que se ha logueado que está guardado en la clase SharedPreferences
         SharedPreferences prefs = getSharedPreferences("datos", Context.MODE_PRIVATE);
         usuario.setText(prefs.getString("usuario", ""));
         contrasena.setText(prefs.getString("contrasena", ""));
 
 
-        //*** LEER JSON: Obtener el fichero json desde la carpeta raw
-        InputStream usu = getResources().openRawResource(R.raw.usuarios);
-        try {
-            // re = readJsonStream(is);
-            parse1 = new ParseJson();
-            listaUsuarios = parse1.readJsonStreamUSUARIO(usu); //recibe un inputstream con el contenido de nuestro fichero .json y nos devolverá nuestra lista formada.
-            System.out.println("Lectura Json terminada");
+        errorUsu = (TextView) findViewById(R.id.txtErrorUsu);
+        errorUsu.setText("");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        TextView et = (TextView) findViewById(R.id.tv_test_json);
-
-        StringBuilder receta = new StringBuilder();
-        // Recorrer objeto List<Usuario> y concatenarlo en una variable para
-        // mostrarlo.
-        receta.append("\nPRUEBAS:: " );
-        for (Usuario u : listaUsuarios) {
-            receta.append("\nDni: " + u.getDni());
-            receta.append("\nNombre: " + u.getNombre());
-
-        }
-
-        et.setText(receta);
-        //***FIN LEER JSON: Obtener el fichero json desde la carpeta raw
 
     }
+
+    // ************************* INICIO CLICK INICIAR SESION ************************************
 
     public void iniciar(View view) {
-        Toast toast1 = Toast.makeText(getApplicationContext(), "botón iniciar pulsado", Toast.LENGTH_SHORT);
-        toast1.show();
+       new ConnectMySql().execute();
+       while(logeado.equals("")){
+        //este while es solamente para esperar la respuesta del proceso del background
 
-        String usu = usuario.getText().toString();
-        String contr = contrasena.getText().toString();
-        Toast notificacion;
-        SharedPreferences preferencias = getSharedPreferences("datos", Context.MODE_PRIVATE);
+       }
+       //si se logea correctamente
+       if(logeado.equals("si")){
 
-        if (usuario.length() < 1) {
-            notificacion = Toast.makeText(this, R.string.invalid_username, Toast.LENGTH_LONG);
-            notificacion.show();
-        }
+           SharedPreferences settings = getSharedPreferences("perfil", MODE_PRIVATE);
+           SharedPreferences.Editor editor = settings.edit();
+           editor.putString("usuarioLogeado", usuario.getText().toString());
+           editor.commit();
+           Intent i = new Intent(this, MainActivity.class);
+           startActivity(i);
+    //si el proceso devuelve un "no"
+       }else{
+           errorUsu.setText("Usuario o contraseña mal introducido");
+       }
 
-        if (contrasena.length() < 1) {
-            notificacion = Toast.makeText(this, R.string.invalid_password, Toast.LENGTH_LONG);
-            notificacion.show();
-        }
-
-        //Se comprueba que el usuario y la contraseña introducidos sean correctos
-        //Usuario
-        for (Usuario u : listaUsuarios) {
-
-            if (u.getDni().equals(usuario.getText().toString())) {
-                usuarioCorrecto = true;
-                usuarioIntroducido = u.getDni().toString();
-                //contraseña
-                if (u.getContrasena().equals(contrasena.getText().toString()) & contrasena.length() >= 1) {
-                    contrasenaCorrecta = true;
-
-                    //Se resetean los datos:
-                    usuario.setText("");
-                    contrasena.setText("");
-
-                    //Si el usuario y la contraseña son correctos se entra en la app
-                    Intent i = new Intent(this, MainActivity.class);
-                    startActivity(i);
-                    //Se guarda el usuario y la contraseña y entra en la app
-                    SharedPreferences settings = getSharedPreferences("perfil", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = settings.edit();
-                    editor.putString("usuarioLogeado", usuarioIntroducido);
-                    editor.commit();
-                    //SharedPreferences.Editor editor=preferencias.edit();
-                    //editor.putString("usuarioLogeado", usuarioIntroducido);
-                    //editor.putString("contrasena", contr);
-                    //editor.commit();
-
-                    setContentView(R.layout.activity_main);
-                } else {
-                    notificacion = Toast.makeText(this, R.string.invalid_password2, Toast.LENGTH_LONG);
-                    notificacion.show();
-                    //Se resetean los datos:
-                    contrasena.setText("");
-                }
-     /*   else{
-            //Se resetean los datos:
-            usuario.setText("");
-            contrasena.setText("");
-            notificacion = Toast.makeText(this, R.string.login_failed, Toast.LENGTH_LONG);
-            notificacion.show();
-        }
-<
-*/
-            }
-        }
     }
+    // **************************** FIN CLICK INICIAR SESION *************************************
+
+    // **************************** INICIO ENCRIPTADO DE CONTRASEÑA ******************************
+
+    public String encriptar(String pass){
+        String value = pass;
+        String sha1 = "";
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.reset();
+            digest.update(value.getBytes("utf8"));
+            sha1 = String.format("%040x", new BigInteger(1, digest.digest()));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return sha1;
+    }
+    // ******************************* FIN ENCRIPTADO DE CONTRASEÑA ******************************
+
+    // ******************************* BOTON IR A REGISTRARSE ************************************
     public void registrarse(View view){
         Intent i = new Intent(this, Registro.class);
         startActivity(i);
     }
+    // ******************************** FIN BOTON REGISTRARSE *************************************
+
+    // ***************************** INICIO PROCESO BASE DE DATOS *********************************
+    private class ConnectMySql extends AsyncTask<String, Void, String>  {
+
+        String res = "";
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+        }
+
+        //proceso para conectar a la base de datos y ver si ese usuario y pass estan bien
+        @SuppressLint("WrongThread")
+        @Override
+        protected String doInBackground(String... variableNoUsada) {
+
+            String usu = usuario.getText().toString();
+            String contr = contrasena.getText().toString();
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+
+
+                String url =  "jdbc:mysql://188.213.5.150:3306/alojamientos?serverTimezone=UTC";
+                //url = "Server=188.213.5.150;Port=3306;Database=alojamientos;Uid=accesoadatos;Pwd=123456;";
+                java.sql.Connection con = DriverManager.getConnection(url,"accesoadatos","123456");
+
+                String query = "SELECT cDni, cContrasena FROM tUsuarios";
+
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery(query);
+
+                while (rs.next()){
+                    String var1 = rs.getString("cDni");
+                    String var2 = rs.getString("cContrasena");
+
+                    if(var1.equals(usu) && var2.equals(encriptar(contr))){
+                        logeado = "si";
+                    }
+                }
+                //si la variable logeado sigue vacia, quiere decir que no ha coincidido los datos introducidos
+                //con lo que hay en la base de datos
+                if(logeado.equals("")){
+                    logeado = "no";
+                }
+
+                rs.close();
+                st.close();
+                con.close();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return res;
+        }
+    }
+    //****************************** FIN PROCESO BASE DE DATOS ***********************************
 }
 
 
