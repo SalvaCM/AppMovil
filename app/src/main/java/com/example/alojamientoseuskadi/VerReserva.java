9bbf750a8e2890fd8125b9d0a13b646e1b7ed3b6
elorrietaa <46441455+elorrietaa@users.noreply.github.com>
2020-01-27 09:42:51 +0100

    se borran las reservas

--- app/src/main/java/com/example/alojamientoseuskadi/VerReserva.java
+++ app/src/main/java/com/example/alojamientoseuskadi/VerReserva.java
@@ -2,15 +2,23 @@ package com.example.alojamientoseuskadi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Calendar;
+import java.util.List;
 
 public class VerReserva extends AppCompatActivity {
     // Declaración de elementos de la activity_ver_reserva.xml
@@ -20,6 +28,7 @@ public class VerReserva extends AppCompatActivity {
     Calendar calendarioEntrada = Calendar.getInstance();
     Calendar calendarioSalida = Calendar.getInstance();
 
+    String codReservaSelec;
 
     TextView nombreUsu;
     TextView apellidoUsu;
@@ -47,7 +56,7 @@ public class VerReserva extends AppCompatActivity {
 
         //Se saca el usuario que se ha logueado que está guardado en la clase SharedPreferences
         SharedPreferences settings = getSharedPreferences("perfil", MODE_PRIVATE);
-        String codReservaSelec = settings.getString("codReservaSelec", "");
+        codReservaSelec = settings.getString("codReservaSelec", "");
         String fechaRealizadaReservaSelec = settings.getString("fechaRealizadaReservaSelec", "");
         String fechaEntradaReservaSelec = settings.getString("fechaEntradaReservaSelec", "");
         String fechaSalidaReservaSelec = settings.getString("fechaSalidaReservaSelec", "");
@@ -109,6 +118,87 @@ public class VerReserva extends AppCompatActivity {
         startActivity(i);
     }
     public void borrarReserva(View view){
+        new ConnectMySqlBBDDDelete().execute(" Estos Strings van a variableNoUsada que no usaremos en este ejemplo y podiamos haber declarado como Void "," si lo necesitaramos podemos cambiar el String por otro tipo de datos "," podemos añadir más de 4 datos que los de este ejemplo, todos los que necesitemos "," y recuerda que se usan como un array, para acceder en concreto a este usaríamos variableNoUsada[3] "); //Arrancamos el AsyncTask. el método "execute" envía datos directamente a doInBackground()
+
+        Intent i = new Intent(this, VerReservas.class);
+        startActivity(i);
+    }
+
+    private class ConnectMySqlBBDDDelete extends AsyncTask<String, Void, List<Reserva>> {
+        private boolean cancelarSiHayMas100Archivos;
+        private ProgressBar miBarraDeProgreso;
+        String res = "";
+
+        /**
+         * Contructor de ejemplo que podemos crear en el AsyncTask
+         *
+         * en este ejemplo le pasamos un booleano que indica si hay más de 100 archivos o no. Si le pasas true se cancela por la mitad del progreso, si le pasas false seguirá hasta el final sin cancelar la descarga simulada
+         */
+        public ConnectMySqlBBDDDelete() {
+
+        }
+        /**
+         * Se ejecuta antes de empezar el hilo en segundo plano. Después de este se ejecuta el método "doInBackground" en Segundo Plano
+         *
+         * Se ejecuta en el hilo: PRINCIPAL
+         */
+        @Override
+        protected void onPreExecute() {
+           // Log.v(TAG_LOG, "ANTES de EMPEZAR la descarga. Hilo PRINCIPAL");
+            super.onPreExecute();
+        }
+
+        /**
+         * Se ejecuta después de "onPreExecute". Se puede llamar al hilo Principal con el método "publishProgress" que ejecuta el método "onProgressUpdate" en hilo Principal
+         *
+         * Se ejecuta en el hilo: EN SEGUNDO PLANO
+         *
+         * param array con los valores pasados en "execute"
+         * @return devuelve un valor al terminar de ejecutar este segundo plano. Se lo envía y ejecuta "onPostExecute" si ha termiado, o a "onCancelled" si se ha cancelado con "cancel"
+         */
+        @SuppressLint("WrongThread")
+        @Override
+        protected List<Reserva> doInBackground(String... variableNoUsada) {
+
+            boolean borrado = false;
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+
+                String url =  "jdbc:mysql://188.213.5.150:3306/alojamientos?serverTimezone=UTC";
+                //url = "Server=188.213.5.150;Port=3306;Database=alojamientos;Uid=accesoadatos;Pwd=123456;";
+                java.sql.Connection con = DriverManager.getConnection(url,"accesoadatos","123456");
+
+                //String query = "SELECT * FROM tReservas";
+
+                //FALTA: and R.cCodUsuario='" & usuarioLogeado.ToString & "'"
+                String query = "DELETE FROM tReservas where cReserva=" + codReservaSelec ;
+                // create the java statement
+                Statement st = con.createStatement();
+
+                // execute the query, and get a java resultset
+                st.execute(query);
+
+                // iterate through the java resultset
+
+                    System.out.format("La reserva se eliminó correctamente.");
+                borrado = true;
+
+                st.close();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                String asd = "";
+            }
+
+            return null;
+        }
+
 
     }
 }
